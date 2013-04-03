fb4432f244faf0de985bc3c79476396a3716dd5a
David Yip <yipdw@northwestern.edu>
2013-04-03 18:03:56 -0500

    Adopt ci-exec.sh pattern.

---
+++ ci-exec.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -xe
+
+if [ -z $RAILS_ENV ]; then
+    export RAILS_ENV='test'
+fi
+
+if [ -z $GEMSET ]; then
+    GEMSET=ncs_navigator_pancakes
+fi
+
+if [ -z $CI_RUBY ]; then
+    echo "CI_RUBY must be set"
+    exit 1
+fi
+
+# Feel free to bump this as new versions of bundler that work with this CI
+# script are released.
+export BUNDLER_VERSION=1.3.2
+
+set +xe
+echo "Initializing RVM"
+source ~/.rvm/scripts/rvm
+set -xe
+
+# On the overnight build, reinstall all gems
+if [ `date +%H` -lt 5 ]; then
+    set +xe
+    echo "Purging gemset to verify that all deps can still be installed"
+    rvm --force $CI_RUBY gemset delete $GEMSET
+    set -xe
+fi
+
+RVM_CONFIG="${CI_RUBY}@${GEMSET}"
+set +xe
+echo "Switching to ${RVM_CONFIG}"
+rvm use $RVM_CONFIG
+set -xe
+
+which ruby
+ruby -v
+
+set +e
+gem list -i bundler -v $BUNDLER_VERSION
+if [ $? -ne 0 ]; then
+  set -e
+  gem install bundler -v $BUNDLER_VERSION
+fi
+set -e
+
+bundle _${BUNDLER_VERSION}_ install
+
+bundle _${BUNDLER_VERSION}_ exec rake --trace
