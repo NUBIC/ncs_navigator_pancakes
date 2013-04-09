1ec44cec42d30632f724ace905c925d76a337537
David Yip <yipdw@northwestern.edu>
2013-04-09 16:44:40 -0500

    Eliminate unnecessary EventTypeStore#load method.

--- app/models/event_type_store.rb
+++ app/models/event_type_store.rb
@@ -4,7 +4,11 @@ class EventTypeStore
   include Celluloid
 
   def request(version = Pancakes::Application.config.mdes_version)
-    @f ||= future(:load, version)
+    @f ||= Celluloid::Future.new do
+      spec = NcsNavigator::Mdes::Specification.new(version)
+      cl = spec.types.detect { |t| t.name == 'event_type_cl1' }.code_list
+      cl.map { |c| EventType.new(c) }.freeze
+    end
   end
 
   def reload(*args)
@@ -14,16 +18,5 @@ class EventTypeStore
 
   def all
     request.value
-  end
-
-  ##
-  # This is public so it can be invoked in a future.  You shouldn't call it,
-  # though.
-  #
-  # @private
-  def load(version)
-    spec = NcsNavigator::Mdes::Specification.new(version)
-    cl = spec.types.detect { |t| t.name == 'event_type_cl1' }.code_list
-    cl.map { |c| EventType.new(c) }.freeze
   end
 end
