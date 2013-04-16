7a6e9fde5a4c82174315b4f2e99a0e729f25ed23
David Yip <yipdw@northwestern.edu>
2013-04-16 16:25:02 -0500

    Cut the navigator.ini ties from Core::Authority.
    Well, maybe.
    
    The user and password will have to be filled in at some point, and
    they'll probably come from navigator.ini, because that's convenient.
    For testing, though, I would prefer to not have that file be necessary.

--- app/models/data_collector_store.rb
+++ app/models/data_collector_store.rb
@@ -7,7 +7,9 @@ class DataCollectorStore
   def initialize
     @authority = NcsNavigator::Authorization::Core::Authority.new(
       :logger => Celluloid.logger,
-      :staff_portal_uri => URI(Pancakes::Application.config.services[:ops])
+      :staff_portal_uri => URI(Pancakes::Application.config.services[:ops]),
+      :staff_portal_user => '',
+      :staff_portal_password => ''
     )
   end
 
