d321aac6a81ab7688bbcc2df77244a03d3744676
David Yip <yipdw@northwestern.edu>
2013-04-23 17:43:00 -0500

    Cases mock: error out on no-param and no-client-ID cases.

--- devel/servers/cases_mock.rb
+++ devel/servers/cases_mock.rb
@@ -11,6 +11,16 @@ abort 'PORT must be set' unless ENV['PORT']
 set :port, ENV['PORT']
 
 get '/api/v1/events' do
+  if params.empty?
+    status 400
+    return ''
+  end
+
+  if request.env['HTTP_X_CLIENT_ID'].nil?
+    status 400
+    return ''
+  end
+
   content_type 'application/json'
   $DATA
 end
