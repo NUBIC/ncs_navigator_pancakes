2a1bca07321cf7b5ef362cffd6bdf6124ec2e460
David Yip <yipdw@northwestern.edu>
2013-05-23 11:09:02 -0500

    Disallow use of default session secret for staging, production, etc.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pancakes::Application.config.secret_token = 'c52be62311eb6d0d1136b2eb23525b296cacecce43b5259ed11820caee6061ec047fc2209c386bc2eeb6d0e4e2450d717cd455dc14e7e63cf5f4c98e53aabf74'
+default_secret = 'pancakes' * 30
+secret_name = 'PANCAKES_SECRET'
+
+if %w(development test ci).include?(Rails.env)
+  Pancakes::Application.config.secret_token = ENV[secret_name] || default_secret
+else
+  Pancakes::Application.config.secret_token = ENV[secret_name] ||
+    fail("#{secret_name} is mandatory for #{Rails.env}")
+end
