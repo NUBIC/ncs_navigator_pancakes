2852d81e7049b93ce299ee8787b8ca253d450d5c
David Yip <yipdw@northwestern.edu>
2013-05-30 16:37:17 -0500

    Permit RAILS_ENV customization on deploy.
    (We don't always deploy to a production environment.  Sometimes it's
    staging.)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 set :application, 'ncs_navigator_pancakes'
 set :deploy_to, ENV['DEPLOY_TO']
 set :keep_releases, 5
+set :rails_env, Rails.env
 set :repository, 'https://github.com/NUBIC/ncs_navigator_pancakes.git'
 set :revision, ENV['REVISION'] || 'origin/master'
 
